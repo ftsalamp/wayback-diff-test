bd99ce8c5db0dca0452a0b8f45fbb80aef8fe50e
Fotis Tsalampounis <ftsalamp@gmail.com>
2018-08-09 17:44:20 +0300

    Use react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React, { Component } from 'react';
 import './App.css';
 import {DiffContainer} from 'wayback-diff';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 class App extends Component {
   render() {
     return (
-        <DiffContainer fetchCallback = {null}/>
+        <Router>
+            <Switch>
+                <Route path="/diff/:timestampA/:timestampB/:site" render={({match}) =>
+                    <DiffContainer site={match.params.site} timestampA={match.params.timestampA}
+                                   timestampB={match.params.timestampB} fetchCallback = {null} />
+                }/>
+                <Route path="/diff/:site" render={({match}) =>
+                    <DiffContainer site={match.params.site} fetchCallback = {null} />} />
+            </Switch>
+        </Router>
     );
   }
 }
