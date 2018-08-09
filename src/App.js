d41abcc711c05c50f88415cc85b1865f291f588f
Fotis Tsalampounis <ftsalamp@gmail.com>
2018-08-09 23:03:33 +0300

    Get webMonitoringProcessing info from props

--- src/App.js
+++ src/App.js
@@ -4,16 +4,24 @@ import {DiffContainer} from 'wayback-diff';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 class App extends Component {
+    webMonitoringProcessingURL= 'http://localhost';
+    webMonitoringProcessingPort= '8888';
+
   render() {
     return (
         <Router>
             <Switch>
                 <Route path="/diff/:timestampA/:timestampB/:site" render={({match}) =>
                     <DiffContainer site={match.params.site} timestampA={match.params.timestampA}
-                                   timestampB={match.params.timestampB} fetchCallback = {null} />
+                      webMonitoringProcessingURL={this.webMonitoringProcessingURL}
+                      webMonitoringProcessingPort={this.webMonitoringProcessingPort}
+                      timestampB={match.params.timestampB} fetchCallback = {null} />
                 }/>
                 <Route path="/diff/:site" render={({match}) =>
-                    <DiffContainer site={match.params.site} fetchCallback = {null} />} />
+                    <DiffContainer site={match.params.site} fetchCallback = {null}
+                     webMonitoringProcessingURL={this.webMonitoringProcessingURL}
+                      webMonitoringProcessingPort={this.webMonitoringProcessingPort}
+                    />} />
             </Switch>
         </Router>
     );
