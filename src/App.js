3d0632e1633e6e3deed051c7424df0e6467dfe79
Fotis Tsalampounis <ftsalamp@gmail.com>
2018-08-10 23:48:56 +0300

    Add limit and waybackLoaderPath props

--- src/App.js
+++ src/App.js
@@ -13,14 +13,16 @@ class App extends Component {
             <Switch>
                 <Route path="/diff/:timestampA/:timestampB/:site" render={({match}) =>
                     <DiffContainer site={match.params.site} timestampA={match.params.timestampA}
-                      webMonitoringProcessingURL={this.webMonitoringProcessingURL}
+                      webMonitoringProcessingURL={this.webMonitoringProcessingURL} limit={'1000'}
                       webMonitoringProcessingPort={this.webMonitoringProcessingPort}
+                      waybackLoaderPath={'https://users.it.teithe.gr/~it133996/wayback-loader.svg'}
                       timestampB={match.params.timestampB} fetchCallback = {null} />
                 }/>
                 <Route path="/diff/:site" render={({match}) =>
                     <DiffContainer site={match.params.site} fetchCallback = {null}
-                     webMonitoringProcessingURL={this.webMonitoringProcessingURL}
-                      webMonitoringProcessingPort={this.webMonitoringProcessingPort}
+                     webMonitoringProcessingURL={this.webMonitoringProcessingURL} limit={'1000'}
+                     waybackLoaderPath={'https://users.it.teithe.gr/~it133996/wayback-loader.svg'}
+                     webMonitoringProcessingPort={this.webMonitoringProcessingPort}
                     />} />
             </Switch>
         </Router>
